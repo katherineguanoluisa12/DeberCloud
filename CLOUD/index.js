3e35a0077116813d258d1835b942105c7c028720
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-07-15 11:14:58 -0500

    Update index.js

--- CLOUD/index.js
+++ CLOUD/index.js
@@ -16,21 +16,3 @@ app.listen(port, () =>{  // ejecuta la api por el puerto 300
 })
 
 
-const express = require('express');
-const app1 = express();
-const proveedoresRoutes = require('./routes/proveedores');
-
-// Middleware para procesar body en formato JSON
-app1.use(express.json());
-
-// Rutas para API de proveedores
-app1.use('/api', proveedoresRoutes);
-
-// Puerto en el que escucha el servidor
-const PORT = process.env.PORT || 3000;
-
-// Iniciar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
-
